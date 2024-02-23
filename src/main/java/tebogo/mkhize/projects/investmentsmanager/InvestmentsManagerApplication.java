ca9f685e82c6a38fcd341d7da85d9261e086b20b
Tebogo Mkhize <tebogomkhize01@gmail.com>
2024-02-23 14:00:51 +0200

    feat: Initialise Spring Boot Application.

---
+++ src/main/java/tebogo/mkhize/projects/investmentsmanager/InvestmentsManagerApplication.java
@@ -0,0 +1,13 @@
+package tebogo.mkhize.projects.investmentsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tebogo/mkhize/projects/investmentsmanager/InvestmentsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tebogo.mkhize.projects.investmentsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
