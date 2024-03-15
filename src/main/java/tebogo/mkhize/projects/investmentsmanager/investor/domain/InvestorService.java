6db430dea0543f29433f5cc795cff54bf2dae08b
Tebogo Mkhize <tebogomkhize01@gmail.com>
2024-03-15 15:47:20 +0200

    feat: Add code that handles adding and retrieving investor(s).
    add code that handles retrieving of all investors and an investor with specified id. Handle investor(s) not found possibility.
    add code that saves investors to database, handle investor already exists (duplicate details) possibility.

--- src/main/java/tebogo/mkhize/projects/investmentsmanager/investor/domain/InvestorService.java
+++ src/main/java/tebogo/mkhize/projects/investmentsmanager/investor/domain/InvestorService.java
@@ -1,7 +1,12 @@
 package tebogo.mkhize.projects.investmentsmanager.investor.domain;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import tebogo.mkhize.projects.investmentsmanager.investor.dto.InvestorResponseDTO;
 
 
 @Service
@@ -11,5 +16,65 @@ public class InvestorService {
     @Autowired
     public InvestorService(InvestorRepository investorRepository) {
         this.investorRepository = investorRepository;
+    }
+
+
+    /**
+     * Retrieves all investors contained within the database provided that
+     * investors are present.
+     * @return InvestorResponseDTO which reflects outcome of query.
+     */
+    public InvestorResponseDTO getInvestors() {
+        List<Investor> investors = (List<Investor>) investorRepository.findAll();
+
+        String outcome = (! investors.isEmpty()) ? "OK": "ERROR";
+        String message = (! investors.isEmpty()) ? "investors retrieved": "investors not found";
+
+        return new InvestorResponseDTO(outcome, message, investors);
+    }
+
+    /**
+     * Retrieves investor with specified id provided that the investor exists.
+     * @param id unique id identifying investor.
+     * @return InvestorResponseDTO which reflects outcome of query.
+     */
+    public InvestorResponseDTO getInvestor(Integer id) {
+        Optional<Investor> investorOpt = investorRepository.findById(id);
+        Investor investor = investorOpt.orElse(null);
+
+        List<Investor> investors = new ArrayList<>();
+        investors.add(investor);
+
+        String outcome = (investor != null) ? "OK": "ERROR";
+
+        String errMsg = "investor with given id " + id + " not found";
+        String message = (investor != null) ? "investor retrieved": errMsg;
+
+        return new InvestorResponseDTO(outcome, message, investors);
+    }
+
+    /**
+     * Adds new investor to database provided that the investor details
+     * aren't already contained within the database.
+     * @param investor Investor instance to be added to the database.
+     * @return InvestorResponseDTO which reflects outcome of query.
+     */
+    public InvestorResponseDTO addInvestor(Investor investor) {
+        List<Investor> investors = investorRepository.findByFirstnameAndLastnameAndContact(
+            investor.getFirstname(), investor.getLastname(),
+            investor.getContact());
+
+        InvestorResponseDTO response;
+        if (investors.isEmpty()) {
+            investorRepository.save(investor);
+            response = getInvestor(investor.getId());
+            response.setMessage("Keep track of generated investor id.");
+
+        } else {
+            response = new InvestorResponseDTO("ERROR", "Investor with " +
+                "provided details already exists", new ArrayList<>());
+        }
+
+        return response;
     }
 }
