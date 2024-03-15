6083f30e6d6b544003a72fcdedab181d99b30ead
Tebogo Mkhize <tebogomkhize01@gmail.com>
2024-03-15 15:40:30 +0200

    feat: Add code to find if investor to be added already exists.
    Code finds if investor with provided firstname, lastname and contact is contained within the database.

--- src/main/java/tebogo/mkhize/projects/investmentsmanager/investor/domain/InvestorRepository.java
+++ src/main/java/tebogo/mkhize/projects/investmentsmanager/investor/domain/InvestorRepository.java
@@ -1,8 +1,11 @@
 package tebogo.mkhize.projects.investmentsmanager.investor.domain;
 
+import java.util.List;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
 
 @Repository
-public interface InvestorRepository extends CrudRepository<Investor, Integer> {}
+public interface InvestorRepository extends CrudRepository<Investor, Integer> {
+    List<Investor> findByFirstnameAndLastnameAndContact(String firstname, String lastname, String contact);
+}
