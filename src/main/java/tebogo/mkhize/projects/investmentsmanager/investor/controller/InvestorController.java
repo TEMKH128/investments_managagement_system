57465845fc6047212b7ebc3d73b0193fd0b136f5
Tebogo Mkhize <tebogomkhize01@gmail.com>
2024-03-15 15:34:39 +0200

    feat: Add Post and Get Mappings for Investments. Add post requests mapping which create a new investor. Add get requests mappings for retrieving all investors present and investor within specified id.

--- src/main/java/tebogo/mkhize/projects/investmentsmanager/investor/controller/InvestorController.java
+++ src/main/java/tebogo/mkhize/projects/investmentsmanager/investor/controller/InvestorController.java
@@ -1,11 +1,16 @@
 package tebogo.mkhize.projects.investmentsmanager.investor.controller;
 
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import tebogo.mkhize.projects.investmentsmanager.investor.domain.InvestorService;
 
+import tebogo.mkhize.projects.investmentsmanager.investor.domain.Investor;
+import tebogo.mkhize.projects.investmentsmanager.investor.domain.InvestorService;
+import tebogo.mkhize.projects.investmentsmanager.investor.dto.InvestorResponseDTO;
+
+// capture error for reqeusts that aren't mapped.
 
 @RestController
+@RequestMapping(path = "investments")
 public class InvestorController {
 
     InvestorService investorService;
@@ -14,4 +19,20 @@ public class InvestorController {
     public InvestorController(InvestorService investorService) {
         this.investorService = investorService;
     }
+
+    @GetMapping("/investors")
+    public InvestorResponseDTO getInvestors() {
+        return investorService.getInvestors();
+    }
+
+    @GetMapping("/investors/{id}")
+    public InvestorResponseDTO getInvestor(@PathVariable Integer id) {
+        return investorService.getInvestor(id);
+    }
+
+    @PostMapping("/investors")
+    public InvestorResponseDTO addInvestor(@RequestBody Investor investor) {
+        return investorService.addInvestor(investor);
+    }
+
 }
