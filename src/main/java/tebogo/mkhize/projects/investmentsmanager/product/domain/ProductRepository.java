8331c1955ed96b27f0b22d9a59a86b599249bfac
Tebogo Mkhize <tebogomkhize01@gmail.com>
2024-03-18 11:22:31 +0200

    feat: add ProductRepository Interface.

---
+++ src/main/java/tebogo/mkhize/projects/investmentsmanager/product/domain/ProductRepository.java
@@ -0,0 +1,7 @@
+package tebogo.mkhize.projects.investmentsmanager.product.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+}
