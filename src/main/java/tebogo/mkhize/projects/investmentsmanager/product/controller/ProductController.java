ba49b30cccacaa5a1bd806eccd2ab0b0698a0615
Tebogo Mkhize <tebogomkhize01@gmail.com>
2024-03-18 11:20:49 +0200

    feat: Create RestController class to handle incoming HTTP requests related to Products.

---
+++ src/main/java/tebogo/mkhize/projects/investmentsmanager/product/controller/ProductController.java
@@ -0,0 +1,11 @@
+package tebogo.mkhize.projects.investmentsmanager.product.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(path = "investments/{id}/products")
+public class ProductController {
+}
+
